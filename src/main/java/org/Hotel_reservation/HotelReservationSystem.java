9e38b18a5cf872cdf1a7ade39ee1167c72ed495c
Nisha7781 <nishamali0781@gmail.com>
2024-03-27 15:50:20 +0530

    Master Created

---
+++ src/main/java/org/Hotel_reservation/HotelReservationSystem.java
@@ -0,0 +1,9 @@
+package org.Hotel_reservation;
+
+public class HotelReservationSystem
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("Welcome to Hotel Reservation System .....");
+    }
+}
