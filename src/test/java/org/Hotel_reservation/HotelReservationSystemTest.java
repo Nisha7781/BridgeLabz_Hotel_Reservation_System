a64f9c64a2d4cae29e4ee843d70a04005661ea61
Nisha7781 <113242714+Nisha7781@users.noreply.github.com>
2024-03-28 14:42:11 +0530

    Find the cheapest hotel for the given dates range

--- src/test/java/org/Hotel_reservation/HotelReservationSystemTest.java
+++ src/test/java/org/Hotel_reservation/HotelReservationSystemTest.java
@@ -15,21 +15,16 @@ public class HotelReservationSystemTest
 
         rs.addHotel("Lakewood");
         rs.addRegularRate("Lakewood", "weekday", 110);
-        rs.addRegularRate("Lakewood", "weekend", 90);
         assertEquals(110, rs.getRegularRate("Lakewood", "weekday"));
-        assertEquals(90, rs.getRegularRate("Lakewood", "weekend"));
+
 
         rs.addHotel("Bridgewood");
         rs.addRegularRate("Bridgewood", "weekday", 160);
-        rs.addRegularRate("Bridgewood", "weekend", 60);
         assertEquals(160, rs.getRegularRate("Bridgewood", "weekday"));
-        assertEquals(60, rs.getRegularRate("Bridgewood", "weekend"));
 
         rs.addHotel("Ridgewood");
         rs.addRegularRate("Ridgewood", "weekday", 220);
-        rs.addRegularRate("Ridgewood", "weekend", 150);
         assertEquals(220, rs.getRegularRate("Ridgewood", "weekday"));
-        assertEquals(150, rs.getRegularRate("Ridgewood", "weekend"));
     }
 
     @Test
@@ -38,15 +33,12 @@ public class HotelReservationSystemTest
 
         rs.addHotel("Lakewood");
         rs.addRegularRate("Lakewood", "weekday", 110);
-        rs.addRegularRate("Lakewood", "weekend", 90);
 
         rs.addHotel("Bridgewood");
         rs.addRegularRate("Bridgewood", "weekday", 160);
-        rs.addRegularRate("Bridgewood", "weekend", 60);
 
         rs.addHotel("Ridgewood");
         rs.addRegularRate("Ridgewood", "weekday", 220);
-        rs.addRegularRate("Ridgewood", "weekend", 150);
 
         assertEquals("Lakewood, Total Rates: $220", rs.findCheapestHotel(LocalDate.of(2020, 9, 10), LocalDate.of(2020, 9, 11)));
     }
