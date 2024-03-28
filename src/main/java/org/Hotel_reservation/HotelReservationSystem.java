6f2eb3ed33fc51c8cf4922556eb1a4aaa9bb65ea
Nisha7781 <nishamali0781@gmail.com>
2024-03-28 17:09:25 +0530

    Added code to find the best rated hotel within given time range

--- src/main/java/org/Hotel_reservation/HotelReservationSystem.java
+++ src/main/java/org/Hotel_reservation/HotelReservationSystem.java
@@ -3,18 +3,22 @@ package org.Hotel_reservation;
 import java.time.*;
 import java.util.*;
 
-public class HotelReservationSystem {
+public class HotelReservationSystem
+{
     private Map<String, Hotel> hotels;
 
-    public HotelReservationSystem() {
+    public HotelReservationSystem()
+    {
         hotels = new HashMap<>();
     }
 
-    public void addHotel(String name, int rating) {
+    public void addHotel(String name, int rating)
+    {
         hotels.put(name, new Hotel(name, rating));
     }
 
-    public void addRate(String hotelName, String dayType, int rate) {
+    public void addRate(String hotelName, String dayType, int rate)
+    {
         Hotel hotel = hotels.get(hotelName);
         if (hotel != null) {
             hotel.setRate(dayType, rate);
@@ -23,10 +27,42 @@ public class HotelReservationSystem {
         }
     }
 
-    public int getTotalRate(Hotel hotel, LocalDate startDate, LocalDate endDate) {
+    public int getRate(String hotelName, String dayType)
+    {
+        Hotel hotel = hotels.get(hotelName);
+        if (hotel != null) {
+            return hotel.getRate(dayType);
+        } else {
+            System.out.println("Hotel " + hotelName + " not found.");
+            return -1;
+        }
+    }
+
+    public String findCheapestBestRatedHotel(LocalDate startDate, LocalDate endDate)
+    {
+        int minTotalRate = Integer.MAX_VALUE;
+        int maxRating = Integer.MIN_VALUE;
+        String cheapestBestRatedHotel = "";
+
+        for (Map.Entry<String, Hotel> entry : hotels.entrySet()) {
+            int totalRate = calculateTotalRate(entry.getValue(), startDate, endDate);
+            int rating = entry.getValue().getRating();
+            if (totalRate < minTotalRate || (totalRate == minTotalRate && rating > maxRating)) {
+                minTotalRate = totalRate;
+                maxRating = rating;
+                cheapestBestRatedHotel = entry.getKey();
+            }
+        }
+
+        return cheapestBestRatedHotel + ", Rating: " + maxRating + " and Total Rates: $" + minTotalRate;
+    }
+
+    private int calculateTotalRate(Hotel hotel, LocalDate startDate, LocalDate endDate)
+    {
         int totalRate = 0;
         LocalDate date = startDate;
-        while (!date.isAfter(endDate)) {
+        while (!date.isAfter(endDate))
+        {
             String dayType = getDayType(date);
             totalRate += hotel.getRate(dayType);
             date = date.plusDays(1);
@@ -34,27 +70,27 @@ public class HotelReservationSystem {
         return totalRate;
     }
 
+
     public String findBestRatedHotel(LocalDate startDate, LocalDate endDate) {
         int maxRating = Integer.MIN_VALUE;
         String bestRatedHotel = "";
-        int totalRates = Integer.MAX_VALUE;
+        int minTotalRate = Integer.MAX_VALUE;
 
-        for (Map.Entry<String, Hotel> entry : hotels.entrySet())
-        {
+        for (Map.Entry<String, Hotel> entry : hotels.entrySet()) {
             Hotel hotel = entry.getValue();
             int rating = hotel.getRating();
-            int totalRate = getTotalRate(hotel, startDate, endDate);
+            int totalRate = calculateTotalRate(hotel, startDate, endDate);
 
-            if (rating > maxRating || (rating == maxRating && totalRate < totalRates))
-            {
+            if (rating > maxRating || (rating == maxRating && totalRate < minTotalRate)) {
                 maxRating = rating;
-                totalRates = totalRate;
-                bestRatedHotel = hotel.getName();
+                minTotalRate = totalRate;
+                bestRatedHotel = hotel.getName() + " & Total Rates $" + minTotalRate;
             }
         }
 
-        return bestRatedHotel + " & Total Rates $" + totalRates;
+        return bestRatedHotel;
     }
+
 
     private String getDayType(LocalDate date)
     {
--- src/test/java/org/Hotel_reservation/HotelReservationSystemTest.java
+++ src/test/java/org/Hotel_reservation/HotelReservationSystemTest.java
@@ -1,12 +1,56 @@
 package org.Hotel_reservation;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.time.LocalDate;
+import java.time.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+public class HotelReservationSystemTest
+{
 
-public class HotelReservationSystemTest {
+    @Test
+    public void testRegularRates()
+    {
+        HotelReservationSystem rs = new HotelReservationSystem();
+
+        rs.addHotel("Lakewood", 3);
+        rs.addRate("Lakewood", "weekday", 110);
+        rs.addRate("Lakewood", "weekend", 90);
+        assertEquals(110, rs.getRate("Lakewood", "weekday"));
+        assertEquals(90, rs.getRate("Lakewood", "weekend"));
+
+        rs.addHotel("Bridgewood", 4);
+        rs.addRate("Bridgewood", "weekday", 150);
+        rs.addRate("Bridgewood", "weekend", 50);
+        assertEquals(150, rs.getRate("Bridgewood", "weekday"));
+        assertEquals(50, rs.getRate("Bridgewood", "weekend"));
+
+        rs.addHotel("Ridgewood", 5);
+        rs.addRate("Ridgewood", "weekday", 220);
+        rs.addRate("Ridgewood", "weekend", 150);
+        assertEquals(220, rs.getRate("Ridgewood", "weekday"));
+        assertEquals(150, rs.getRate("Ridgewood", "weekend"));
+    }
+
+    @Test
+    public void testFindCheapestBestRatedHotel() {
+        HotelReservationSystem rs = new HotelReservationSystem();
+
+        rs.addHotel("Lakewood", 3);
+        rs.addRate("Lakewood", "weekday", 110);
+        rs.addRate("Lakewood", "weekend", 90);
+
+        rs.addHotel("Bridgewood", 4);
+        rs.addRate("Bridgewood", "weekday", 150);
+        rs.addRate("Bridgewood", "weekend", 50);
+
+        rs.addHotel("Ridgewood", 5);
+        rs.addRate("Ridgewood", "weekday", 220);
+        rs.addRate("Ridgewood", "weekend", 150);
+
+        assertEquals("Bridgewood, Rating: 4 and Total Rates: $200", rs.findCheapestBestRatedHotel(LocalDate.of(2020, 9, 11), LocalDate.of(2020, 9, 12)));
+    }
+
     @Test
     public void testFindBestRatedHotel() {
         HotelReservationSystem rs = new HotelReservationSystem();
