6637e049a7db8ef51bffa5764b46193188c3e764
Nisha7781 <113242714+Nisha7781@users.noreply.github.com>
2024-03-28 14:40:54 +0530

    Add a hotel with name and rates for regular customers

--- src/test/java/org/Hotel_reservation/HotelReservationSystemTest.java
+++ src/test/java/org/Hotel_reservation/HotelReservationSystemTest.java
@@ -13,20 +13,16 @@ public class HotelReservationSystemTest
 
         rs.addHotel("Lakewood");
         rs.addRegularRate("Lakewood", "weekday", 110);
-        rs.addRegularRate("Lakewood", "weekend", 90);
         assertEquals(110, rs.getRegularRate("Lakewood", "weekday"));
-        assertEquals(90, rs.getRegularRate("Lakewood", "weekend"));
+
 
         rs.addHotel("Bridgewood");
         rs.addRegularRate("Bridgewood", "weekday", 160);
-        rs.addRegularRate("Bridgewood", "weekend", 60);
         assertEquals(160, rs.getRegularRate("Bridgewood", "weekday"));
-        assertEquals(60, rs.getRegularRate("Bridgewood", "weekend"));
+
 
         rs.addHotel("Ridgewood");
         rs.addRegularRate("Ridgewood", "weekday", 220);
-        rs.addRegularRate("Ridgewood", "weekend", 150);
         assertEquals(220, rs.getRegularRate("Ridgewood", "weekday"));
-        assertEquals(150, rs.getRegularRate("Ridgewood", "weekend"));
     }
 }
